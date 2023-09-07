7f269fc1fda8cf7194c403d42ae6284ee8f3237a
JAAR <joe.aaron.acosta@outlook.com>
2023-09-07 14:03:27 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 09/06/2023
+>>> Practicing Fundamentals Program 21
+>>> Version 0
+'''
+
+'''
+>>> Prompts the user to create a new file, rename an existing file, or to check if a file they are trying to open exists.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
