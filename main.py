8ddd5f9fbe37cd5aea5b9eb40b662f7ba26d62bf
JAAR <joe.aaron.acosta@outlook.com>
2023-09-11 10:21:17 -0400

    Version 2

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 >>> JAAR
 >>> 09/11/2023
 >>> Practicing Fundamentals Program 21
->>> Version 1
+>>> Version 2
 '''
 
 '''
 >>> Prompts the user to create a new file, rename an existing file, or to check if a file they are trying to open exists.
+
+>>> Append: Asks the user to enter a file, if the file exists, deletes the file or lets the user know the file doesn't exist.
 '''
 
 import os
+import re
 
 def user_response(question)->str :
     '''
@@ -29,11 +32,12 @@ def user_action()-> int :
     >>> Asks the user to enter an action they'd like to take. If the user enters an invalid response, prompts the user to enter another response.
     >>> Return: (int) action
     '''
-    actions = [1, 2, 3]
+    actions = [1, 2, 3, 4]
     print('''
         (1) Create a file.
         (2) Check if a file exists.
-        (3) Change the name of an existing file.
+        (3) Rename an existing file.
+        (4) Delete a file.
     ''')
     action = ''
     while action not in actions :
@@ -46,35 +50,45 @@ def user_action()-> int :
         else :
             return action
 
-def file_name_input(string = ' ') :
+def file_path_input(string = ' ', path = 'path') :
     '''
     >>> Asks the user to input a file name. Then asks the user to verify their input before it returns the user input.
     >>> Return: (file) file_name
     '''
     while True :
-        file_name = input(f'Enter a{string}file name: ')
-        print(f"Your input was: '{file_name}'")
+        file_path = input(f'Enter a{string}file {path}: ')
+        print(f"Your input was: '{file_path}'")
         response = user_response('Is your input correct')
         if response == 'yes' :
-            return file_name
+            return file_path
 
 def main() :
     response = 'yes'
     while response == 'yes' :
         action = user_action()
-        file = file_name_input()
+        file_name = ''
         if action == 1 :
-            with open(f'{file}.txt', 'w') as f:
-                print(f'{file}.txt was created ')
-        elif action == 2 :
-            try :
-                with open(file, 'r') as f :
-                    print(f'file {f.name()} exists.')
-            except IOError as e :
-                print(f"file {file} doesn't exist.")
+            file_name = file_path_input(path = 'name')
         else :
-            rename = file_name_input(string = ' new ')
-            os.rename(file, f'{rename}.txt')
+            file_path = file_path_input()
+            file_name = re.search(r'[a-zA-Z0-9._-]+\.txt', file_path).group()
+        if action == 1 :
+            with open(f'{file_name}.txt', 'w') as f:
+                print(f'{file_name}.txt was created ')
+        elif action == 2 :
+            if os.path.exists(file_path) :
+                print(f'{file_name} exists.')
+            else :
+                print(f"{file_name} doesn't exist.")
+        elif action == 3 :
+            rename = file_path_input(string = ' new ')
+            os.rename(file_path, f'{rename}.txt')
+        else :
+            try :
+                os.remove(file_path)
+                print(f'{file_name} was deleted.')
+            except OSError :
+                print(f"{file_name} doesn't exist.")
         response = user_response('Do you want to take another action?')
 
 if __name__ == '__main__' :
